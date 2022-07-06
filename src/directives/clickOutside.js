2d2e649add8f9da4aefd410e74244cd0c6166d3c
Alexandr Zhavoronkov <sasazhava13@gmail.com>
2022-07-06 17:22:48 +0300

    fix log

--- src/directives/clickOutside.js
+++ src/directives/clickOutside.js
@@ -1,6 +1,5 @@
 const clickOutside = {
   beforeMount: (el, binding) => {
-    console.log(el, binding)
     el.clickOutsideEvent = event => {
       if (!(el == event.target || el.contains(event.target))) {
         binding.value();
